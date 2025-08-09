e238d264e656921f78f2ca7520ac2637fd78da88
sanjayntms <sanjayntms@yahoo.com>
2025-08-09 21:25:54 +0530

    Update variables.tf

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,4 +1,4 @@
-variable "resource_group_name" { type = string }
-variable "storage_account_name" { type = string }
-variable "keyvault_name" { type = string }
-variable "location" { type = string }
+# variable "resource_group_name" { type = string }
+# variable "storage_account_name" { type = string }
+# variable "keyvault_name" { type = string }
+# variable "location" { type = string }
