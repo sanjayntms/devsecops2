5d9faf37bffd6390eecb509b5ee8fa9727725f1a
sanjayntms <sanjayntms@yahoo.com>
2025-08-09 10:15:20 +0530

    Update variables.tf

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,4 +1,4 @@
-variable "vm_password" {
-  type      = string
-  sensitive = true
-}
+variable "resource_group_name" { type = string }
+variable "storage_account_name" { type = string }
+variable "keyvault_name" { type = string }
+variable "location" { type = string }
