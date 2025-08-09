8f50b0fb1e197cbd7927659d7eba1e2239bc3fab
sanjayntms <sanjayntms@yahoo.com>
2025-08-09 10:18:40 +0530

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "devsecops-rg"
-    storage_account_name = "yourterraformsa"
-    container_name       = "tfstate"
-    key                  = "webvm.terraform.tfstate"
+    resource_group_name   = var.resource_group_name
+    storage_account_name  = var.storage_account_name
+    container_name        = "tfstate"
+    key                   = "terraform.tfstate"
   }
 }
 
@@ -11,77 +11,89 @@ provider "azurerm" {
   features {}
 }
 
+data "azurerm_client_config" "current" {}
+
+variable "resource_group_name" { type = string }
+variable "storage_account_name" { type = string }
+variable "keyvault_name" { type = string }
+variable "location" { type = string }
+
 resource "azurerm_resource_group" "rg" {
-  name     = "devsecops-rg"
-  location = "Central India"
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_storage_account" "sa" {
+  name                     = var.storage_account_name
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "tfstate" {
+  name                  = "tfstate"
+  storage_account_name  = azurerm_storage_account.sa.name
+  container_access_type = "private"
+}
+
+resource "azurerm_key_vault" "kv" {
+  name                        = var.keyvault_name
+  location                    = azurerm_resource_group.rg.location
+  resource_group_name         = azurerm_resource_group.rg.name
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  sku_name                    = "standard"
+}
+
+data "azurerm_key_vault_secret" "vm_password" {
+  name         = "webvm-password"
+  key_vault_id = azurerm_key_vault.kv.id
 }
 
 resource "azurerm_virtual_network" "vnet" {
-  name                = "devsecops-vnet"
+  name                = "web-vnet"
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 }
 
-resource "azurerm_subnet" "subnet" {
+resource "azurerm_subnet" "websubnet" {
   name                 = "web-subnet"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
 
-resource "azurerm_public_ip" "webip" {
-  name                = "web-public-ip"
+resource "azurerm_public_ip" "webvm" {
+  name                = "webvm-pip"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  allocation_method   = "Static"
+  allocation_method   = "Dynamic"
 }
 
 resource "azurerm_network_interface" "nic" {
-  name                = "web-nic"
+  name                = "webvm-nic"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
   ip_configuration {
     name                          = "internal"
-    subnet_id                     = azurerm_subnet.subnet.id
+    subnet_id                     = azurerm_subnet.websubnet.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.webip.id
+    public_ip_address_id          = azurerm_public_ip.webvm.id
   }
 }
 
-resource "azurerm_network_security_group" "nsg" {
-  name                = "web-nsg"
-  location            = azurerm_resource_group.rg.location
+resource "azurerm_linux_virtual_machine" "webvm" {
+  name                = "webvm"
   resource_group_name = azurerm_resource_group.rg.name
-
-  security_rule {
-    name                       = "AllowHTTP"
-    priority                   = 100
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "80"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-}
-
-resource "azurerm_network_interface_security_group_association" "assoc" {
-  network_interface_id      = azurerm_network_interface.nic.id
-  network_security_group_id = azurerm_network_security_group.nsg.id
-}
-
-resource "azurerm_linux_virtual_machine" "vm" {
-  name                            = "webvm"
-  location                        = azurerm_resource_group.rg.location
-  resource_group_name             = azurerm_resource_group.rg.name
-  network_interface_ids           = [azurerm_network_interface.nic.id]
-  size                            = "Standard_B1s"
-  admin_username                  = "azureuser"
-  disable_password_authentication = false
-  admin_password                  = var.vm_password
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_B1s"
+  admin_username      = "azureuser"
+  admin_password      = data.azurerm_key_vault_secret.vm_password.value
+  network_interface_ids = [
+    azurerm_network_interface.nic.id,
+  ]
 
   os_disk {
     caching              = "ReadWrite"
@@ -90,14 +102,9 @@ resource "azurerm_linux_virtual_machine" "vm" {
 
   source_image_reference {
     publisher = "Canonical"
-    offer     = "0001-com-ubuntu-server-jammy"
-    sku       = "22_04-lts-gen2"
+    offer     = "0001-com-ubuntu-server-focal"
+    sku       = "20_04-lts"
     version   = "latest"
   }
-
-  computer_name = "webvm"
 }
 
-output "web_vm_ip" {
-  value = azurerm_public_ip.webip.ip_address
-}
