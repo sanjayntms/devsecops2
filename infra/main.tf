24078b563db76c470bbb19d1e372f85babf89249
sanjayntms <sanjayntms@yahoo.com>
2025-08-10 09:59:41 +0530

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -23,19 +23,7 @@ resource "azurerm_resource_group" "rg" {
   location = var.location
 }
 
-resource "azurerm_storage_account" "sa" {
-  name                     = var.storage_account_name
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-}
 
-resource "azurerm_storage_container" "tfstate" {
-  name                  = "tfstate"
-  storage_account_name  = azurerm_storage_account.sa.name
-  container_access_type = "private"
-}
 
 resource "azurerm_key_vault" "kv" {
   name                        = var.keyvault_name
