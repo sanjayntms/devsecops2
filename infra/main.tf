8b8477faf11431a0c0d10bb5bde961b485780f99
sanjayntms <sanjayntms@yahoo.com>
2025-08-10 11:32:44 +0530

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -102,7 +102,7 @@ resource "azurerm_linux_virtual_machine" "webvm" {
 }
 # Network Security Group
 resource "azurerm_network_security_group" "webvm_nsg" {
-  name                = "${var.vm_name}-nsg"
+  name                = "webvm-nsg"
   location            = data.azurerm_resource_group.rg.location
   resource_group_name = data.azurerm_resource_group.rg.name
 
